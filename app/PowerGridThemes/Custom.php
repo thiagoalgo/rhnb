746c51881b4eb5479020fc5f9bb1ebb7694d2443
Thiago Goulart <thiagoalgo@gmail.com>
2023-12-21 19:28:50 -0300

    feat: adjusted font size in power grid custom theme

--- app/PowerGridThemes/Custom.php
+++ app/PowerGridThemes/Custom.php
@@ -38,7 +38,7 @@ class Custom extends ThemeBase
             ->th('font-semibold px-2 pr-4 py-3 text-left text-xs font-semibold text-pg-primary-700 tracking-wider whitespace-nowrap dark:text-pg-primary-300')
             ->tbody('text-pg-primary-800')
             ->trBody('border-b border-pg-primary-100 dark:border-pg-primary-600 hover:bg-pg-primary-50 dark:bg-pg-primary-800 dark:hover:bg-pg-primary-700')
-            ->tdBody('pl-[19px] px-3 py-2 whitespace-nowrap dark:text-pg-primary-200')
+            ->tdBody('pl-[19px] px-3 py-2 whitespace-nowrap dark:text-pg-primary-200 text-sm')
             ->tdBodyEmpty('px-3 py-2 whitespace-nowrap dark:text-pg-primary-200')
             ->trBodyClassTotalColumns('!bg-red-800')
             ->tdBodyTotalColumns('px-3 py-2 whitespace-nowrap dark:text-pg-primary-200 text-sm text-pg-primary-600 text-right space-y-2');
