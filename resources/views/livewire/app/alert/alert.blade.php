43633a76b5994de2a91f97605924594a7db6eca1
Thiago Goulart <thiagoalgo@gmail.com>
2023-12-29 20:34:41 -0300

    fix: wrappered alert to max width

--- resources/views/livewire/app/alert/alert.blade.php
+++ resources/views/livewire/app/alert/alert.blade.php
@@ -1,28 +1,38 @@
 <div>
-    @if (session('info'))
-        <div class="p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400 shadow" role="alert">
-            {{ session('info') }}
-        </div>
-    @endif
-    @if (session('danger'))
-        <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 shadow" role="alert">
-            {{ session('danger') }}
-        </div>
-    @endif
-    @if (session('success'))
-        <div class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400 shadow" role="alert">
-            {{ session('success') }}
-        </div>
-    @endif
-    @if (session('warning'))
-        <div class="p-4 mb-4 text-sm text-yellow-800 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300" shadow
-            role="alert">
-            {{ session('warning') }}
-        </div>
-    @endif
-    @if (session('dark'))
-        <div class="p-4 text-sm text-gray-800 rounded-lg bg-gray-50 dark:bg-gray-800 dark:text-gray-300 shadow" role="alert">
-            {{ session('dark') }}
+    @if (session('info') or session('danger') or session('success') or session('warning') or session('dark'))
+        <div class="flex justify-center gap-4 mb-2">
+            <div class="flex w-full max-w-screen-xl flex-col">
+                @if (session('info'))
+                    <div class="p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400 shadow"
+                        role="alert">
+                        {{ session('info') }}
+                    </div>
+                @endif
+                @if (session('danger'))
+                    <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 shadow"
+                        role="alert">
+                        {{ session('danger') }}
+                    </div>
+                @endif
+                @if (session('success'))
+                    <div class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400 shadow"
+                        role="alert">
+                        {{ session('success') }}
+                    </div>
+                @endif
+                @if (session('warning'))
+                    <div class="p-4 mb-4 text-sm text-yellow-800 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300"
+                        shadow role="alert">
+                        {{ session('warning') }}
+                    </div>
+                @endif
+                @if (session('dark'))
+                    <div class="p-4 text-sm text-gray-800 rounded-lg bg-gray-50 dark:bg-gray-800 dark:text-gray-300 shadow"
+                        role="alert">
+                        {{ session('dark') }}
+                    </div>
+                @endif
+            </div>
         </div>
     @endif
 </div>
