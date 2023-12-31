b934e75130928afb97acc690189f983014ffcc18
Thiago Goulart <thiagoalgo@gmail.com>
2023-12-31 14:56:00 -0300

    feat: card to show profile information in profile edit created

--- resources/views/livewire/profile/profile-form.blade.php
+++ resources/views/livewire/profile/profile-form.blade.php
@@ -1,9 +1,48 @@
 <div class="flex justify-center">
-    <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 w-full max-w-screen-xl">
+
+
+
+
+    <div class="grid grid-cols-1 xl:grid-cols-1 gap-4 w-full max-w-screen-xl">
+
 
         <x-custom.card class="w-full">
+            <x-custom.form-title text="{{ __('Employee') }}" />
+
+            <div class="mt-2">
+                <dl class="divide-y divide-gray-100">
+                    <div class="px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0 lg:grid-cols-6">
+                        <dt class="text-sm font-medium leading-6 text-gray-900">{{ __('Name') }}</dt>
+                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                            {{ $form->employee->user->name }}</dd>
+                    </div>
+                    <div class="px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0 lg:grid-cols-6">
+                        <dt class="text-sm font-medium leading-6 text-gray-900">{{ __('Email') }}</dt>
+                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                            {{ $form->employee->user->email }}</dd>
+                    </div>
+                    <div class="px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0 lg:grid-cols-6">
+                        <dt class="text-sm font-medium leading-6 text-gray-900">{{ __('Registration') }}</dt>
+                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                            {{ $form->employee->registration }}</dd>
+                    </div>
+                    <div class="px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0 lg:grid-cols-6">
+                        <dt class="text-sm font-medium leading-6 text-gray-900">{{ __('Job Title') }}</dt>
+                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                            {{ $form->employee->jobTitle->name }}</dd>
+                    </div>
+                    <div class="px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0 lg:grid-cols-6">
+                        <dt class="text-sm font-medium leading-6 text-gray-900">{{ __('Department') }}</dt>
+                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                            {{ $form->employee->department->name }}</dd>
+                    </div>
+                </dl>
+            </div>
+        </x-custom.card>
+
+        <x-custom.card>
             <form wire:submit="save">
-                <x-custom.form-title text="{{ __('Profile Informations') }}" />
+                <x-custom.form-title text="{{ __('Edit') . ' ' . __('Profile Informations') }}" />
 
                 <div class="grid grid-cols-1 gap-2 lg:gap-4">
 
@@ -25,7 +64,7 @@
             </form>
         </x-custom.card>
 
-        <x-custom.card class="flex">
+        <x-custom.card>
             <form wire:submit="changePassword">
                 <x-custom.form-title text="{{ __('Change Password') }}" />
 
@@ -42,7 +81,7 @@
 
                 </div>
 
-                <div class="grid grid-cols-2 lg:grid-cols-2 mt-6">
+                <div class="mt-6">
                     <div>
                         <x-button text="{{ __('Change Password') }}" />
                     </div>
